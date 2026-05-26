4fe5ee0414c2ab478c1bd7f02dddaf0b577730c8
Gaspard Deltour Bauregard <gaspard.deltourbauregard@epfl.ch>
2026-05-26 22:04:03 +0200

    feat:Implemented the losses and activations

--- code_skeleton_m2/code_skeleton_m2/src/losses.py
+++ code_skeleton_m2/code_skeleton_m2/src/losses.py
@@ -6,9 +6,15 @@ class MSE:
         :param y_pred: (array) Prediction vector
         :return: (flt)
         """
-        ### WRITE YOUR CODE HERE
+        totalLoss = 0
+        for i in range(len(y_true)):
+            totalLoss += (y_true[i] -y_pred[i])**2
+        return totalLoss/len(y_true)
 
     @staticmethod
     def gradient(y_true, y_pred):
-        ### WRITE YOUR CODE HERE
-        return
+        totalLoss = 0
+        gradiant = []
+        for i in range(len(y_true)):
+            gradiant[i] =( 2 * (y_pred[i] - y_true[i]))/ len(y_true)
+        return gradiant
